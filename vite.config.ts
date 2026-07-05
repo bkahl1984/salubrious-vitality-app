f70a624f273b2018a1b5008426e972880e4a8086
Brad Kahl <bradkahl@Brads-MacBook-Pro-2.local>
2026-07-05 16:29:30 -0400

    update vite config to point to correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/salubrious-vitality-app/",
+  // base: "/salubrious-vitality-app/", // built this way before using cname domain
+  base: "/",
 })
